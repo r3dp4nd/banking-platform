eb881524e88540a8d5b25c162b7e780b0719a6cb
V Olek MP <olek.mp23@gmail.com>
2026-08-03 19:31:14 -0500

    feat: init base project

---
+++ src/test/java/com/olek/banking/BankingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olek.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
